63cc23fbbd7ae9986529bcf74590f78596c6c75b
Yeon <bsuyeon16@gamil.com>
2018-04-11 11:27:56 +0900

    Spring - @Resource Annotation으로 수정 20180411

--- carmaker/src/main/java/edu/biz/ioc4/OrderManager.java
+++ carmaker/src/main/java/edu/biz/ioc4/OrderManager.java
@@ -1,16 +1,17 @@
 package edu.biz.ioc4;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service("orderManager")
 public class OrderManager {
-	@Autowired //TYPE이 CarMaker인 것 찾아 자동 주입, 여러개인 경우 Qualifier로 지정
-	@Qualifier("daewoo")
+	@Resource(name="daewoo")
 	private CarMaker maker;
 	
-	@Autowired //TYPE이 Money인 것을 찾아 자동 주입
+	@Resource(name="money") //TYPE이 Money인 것을 찾아 자동 주입
 	private Money money;
 	
 	public OrderManager() {}
