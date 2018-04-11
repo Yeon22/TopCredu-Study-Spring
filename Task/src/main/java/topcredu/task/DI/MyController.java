4ce7289c3e6a700b74810e0c6c7603f9cdbd1631
Yeon <bsuyeon16@gamil.com>
2018-04-11 10:37:38 +0900

    4/10일 과제 3번 수정... 여전히 안됌. 20180411

--- Task/src/main/java/topcredu/task/DI/MyController.java
+++ Task/src/main/java/topcredu/task/DI/MyController.java
@@ -3,15 +3,16 @@ package topcredu.task.DI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-	private ProductService productService;
+	private ProductServicelmpl productService;
 	
-	@Autowired
+	@Qualifier
 	public void setProductService(ProductService productService) {
-		this.productService = productService;
+		this.productService = (ProductServicelmpl) productService;
 	}
 	
 	public List<Product> getProducts(){
