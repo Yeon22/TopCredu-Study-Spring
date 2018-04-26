66d8d1528818d0ee43b6a450b09b4af677bc807c
yeon <bsuyeon16@gmail.com>
2018-04-26 20:15:21 +0900

    충고가 나오기는 하나 DB연결할 때 handle오류가 남... 20180426

--- SpringLegacyMyBatisBoard/src/main/java/com/example/advice/MyAdvice.java
+++ SpringLegacyMyBatisBoard/src/main/java/com/example/advice/MyAdvice.java
@@ -5,22 +5,21 @@ import org.aspectj.lang.ProceedingJoinPoint;
 
 public class MyAdvice {
 	public void before(JoinPoint joinPoint) {
-		System.out.println(joinPoint.getSignature().getName() + "(" + joinPoint.getArgs()[0] + ") : 사전충고");
+		System.out.println(joinPoint.getSignature().getName() + " : 사전충고");
 	}
 	
 	public void afterReturning(JoinPoint joinPoint) {
-		System.out.println(joinPoint.getSignature().getName() + "(" + joinPoint.getArgs()[0] + ") : 사후충고");
+		System.out.println(joinPoint.getSignature().getName() + " : 사후충고");
 	}
 	
 	public Object around(ProceedingJoinPoint pjp) throws Throwable {
-		String findName = (String)pjp.getSignature().getName();
-		String methodName = (String)pjp.getArgs()[0];
 		long start = System.currentTimeMillis();
-		long end = System.currentTimeMillis();
+		System.out.println("[주변충고] 시작 시간 : " + start/1000.0 + " 초");
 		
-		System.out.println("[주변충고] " + findName + ", 메소드 이름 : " + methodName + "시작 시간 : " + start/1000.0 + "초");
 		Object obj = pjp.proceed();
-		System.out.println("[주변충고] " + findName + ", 메소드 이름 : " + methodName + "종료 시간 : " + end/1000.0 + "초");
+		
+		long end = System.currentTimeMillis();
+		System.out.println("[주변충고] 종료 시간 : " + end/1000.0 + " 초");
 		
 		
 		return obj;
