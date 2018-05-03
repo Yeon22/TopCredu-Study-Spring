8257af54dbfd6077b0c9ec37de8bb561e15b8152
yeon <bsuyeon16@gmail.com>
2018-05-03 10:58:49 +0900

    Spring Web MVC Reivew - @CookieValue 실습 20180503

--- 20180503 - springmvcexception/src/main/java/a/b/springmvcexception/HomeController.java
+++ 20180503 - springmvcexception/src/main/java/a/b/springmvcexception/HomeController.java
@@ -2,7 +2,11 @@ package a.b.springmvcexception;
 
 import java.io.IOException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +23,19 @@ public class HomeController {
 		{
 			throw new IOException("occured IOException...");
 		}
+	}
+	
+	@RequestMapping(value = "/hello.html")
+	public String hello2(@CookieValue(value = "onjCounter", defaultValue = "0") Integer onjCounter, HttpServletResponse response) {
+		//쿠키값을 일어 우선 counter, 1증가
+		onjCounter++;
+		
+		//응답에 쿠키를 셋팅
+		Cookie cookie = new Cookie("onjCounter", onjCounter.toString());
+		response.addCookie(cookie);
+		
+		//view이름
+		return "cookie";
 	}
 	
 }
---
+++ 20180503 - springmvcexception/src/main/webapp/WEB-INF/views/cookie.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>Spring MVC Cookie example</title>
+</head>
+<body>
+	<h1>OracleJavaCommunity</h1>
+	<h1>Spring Cookie Example</h1>
+	Page hit counter : <b>${cookie.onjCounter.value}</b>
+</body>
+</html>
