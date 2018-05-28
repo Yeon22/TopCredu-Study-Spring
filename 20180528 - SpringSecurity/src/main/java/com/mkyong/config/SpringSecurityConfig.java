9c7c6708c74560ea817513b3ace1b254f778d7a5
yeon <bsuyeon16@gmail.com>
2018-05-28 16:33:30 +0900

    Spring Security 연습 mkyong.com 20180528

--- 20180528 - SpringSecurity/src/main/java/com/mkyong/config/SpringSecurityConfig.java
+++ 20180528 - SpringSecurity/src/main/java/com/mkyong/config/SpringSecurityConfig.java
@@ -44,9 +44,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
+                .withUser("user").password("{noop}password").roles("USER")
                 .and()
-                .withUser("admin").password("password").roles("ADMIN");
+                .withUser("admin").password("{noop}password").roles("ADMIN");
     }
 
     /*
